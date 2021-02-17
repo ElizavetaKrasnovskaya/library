1546dce260612f56320bd6dcfb514cff92c51372
Elizaveta <ElizavetaKrasnovskaya@gmail.com>
2021-02-17 11:02:37 +0300

    controller

---
+++ src/main/java/com/epam/library/controller/ApplicationController.java
@@ -0,0 +1,42 @@
+package com.epam.library.controller;
+
+
+import com.epam.library.command.Command;
+import com.epam.library.command.ResponseContext;
+import com.epam.library.command.WrappingRequestContext;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/controller")
+public class ApplicationController extends HttpServlet {
+
+    private static final String COMMAND_PARAMETER_NAME = "command";
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        process(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        process(req, resp);
+    }
+
+    private void process(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        final String commandName = req.getParameter(COMMAND_PARAMETER_NAME);
+        final Command businessCommand = Command.of(commandName);
+        final ResponseContext result = businessCommand.execute(WrappingRequestContext.of(req));
+        if (result.isRedirect()) {
+            //todo
+        } else {
+            final RequestDispatcher dispatcher = req.getRequestDispatcher(result.getPage());
+            dispatcher.forward(req, resp);
+        }
+    }
+}
